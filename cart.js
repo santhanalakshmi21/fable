2a597be035797efc6e0784e620f2b7931055e16b
Santhanalakshmi <104186416+santhanalakshmi21@users.noreply.github.com>
2023-05-28 02:31:07 +0530

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,20 @@
+var minusButton = document.getElementsByClassName("btn minus1")
+for (var i = 0; i < minusButton.length; i++) {
+minusButton[i].addEventListener("click", function(event){
+var quantity = document.getElementsByClassName("cart-quantity-input")
+for (var i = 0; i < quantity.length; i++) {
+var quantity = quantity[i].value--
+//console.log(quantity)
+ }
+})}
+var minusButton = document.getElementsByClassName("btn minus1")
+for (var i = 0; i < minusButton.length; i++) {
+minusButton[i].addEventListener("click", function(event){
+var quantity = document.getElementsByClassName("cart-quantity-input")
+for (var i = 0; i < quantity.length; i++) {
+var quantity = quantity[i].value--
+//console.log(quantity)
+ }
+})}
+
+ 
